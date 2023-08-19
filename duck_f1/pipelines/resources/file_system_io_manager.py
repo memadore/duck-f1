a460faf202fa8e0bfa1d303136095bdcae7480b2
Marc-Etienne Madore <marc-etienne.madore@cleo.eco>
2023-08-19 14:39:00 -0400

    chore(pipeline): added output context to arrow-parquet io manager

--- duck_f1/pipelines/resources/file_system_io_manager.py
+++ duck_f1/pipelines/resources/file_system_io_manager.py
@@ -1,6 +1,8 @@
+import os
+
 import pyarrow as pa
 import pyarrow.parquet as pq
-from dagster import InputContext, OutputContext, UPathIOManager
+from dagster import InputContext, MetadataValue, OutputContext, UPathIOManager
 from upath import UPath
 
 
@@ -11,6 +13,34 @@ class ArrowParquetIOManager(UPathIOManager):
         with path.open("wb") as file:
             pq.write_table(obj, file)
 
+        output_metadata = {
+            "table_size": obj.nbytes,
+            "table_size_label": self._compute_size_label(obj.nbytes),
+            "col_count": obj.num_columns,
+            "row_count": obj.num_rows,
+        }
+
+        if os.getenv("ENV", "prod") == "dev":
+            output_metadata["preview"] = MetadataValue.md(
+                obj.slice(length=5).to_pandas().to_markdown()
+            )
+
+        context.add_output_metadata(output_metadata)
+
     def load_from_path(self, context: InputContext, path: UPath) -> pa.Table:
         with path.open("rb") as file:
             return pq.read_table(file)
+
+    @staticmethod
+    def _compute_size_label(length: int, decimals: int = 2) -> float:
+        scales = {
+            "B": 1,
+            "KB": 1e-3,
+            "MB": 1e-6,
+            "GB": 1e-9,
+            "TB": 1e-12,
+        }
+        for label, scale in scales.items():
+            if length * scale < 1000:
+                value = round(length * scale, decimals)
+                return f"{value} {label}"
