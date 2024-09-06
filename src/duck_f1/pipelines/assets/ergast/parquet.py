fc68c8716139899c3d90c7c3627de0c73b84ac4c
Marc-Etienne Madore <17458989+memadore@users.noreply.github.com>
2024-09-05 21:34:43 -0400

    fix: add manual schema for pitstops

--- src/duck_f1/pipelines/assets/ergast/parquet.py
+++ src/duck_f1/pipelines/assets/ergast/parquet.py
@@ -3,8 +3,10 @@ import urllib.request
 import zipfile
 from typing import Dict, List
 
+import pyarrow as pa
 from dagster import AssetExecutionContext, AssetOut, Output, multi_asset
 from pyarrow import Table, csv
+from pyarrow.csv import ConvertOptions
 
 from .config import ErgastAsset
 
@@ -19,11 +21,32 @@ def parquet_asset_factory(
         compute_kind="python",
     )
     def _parquet_files(context: AssetExecutionContext) -> Table:
+        schemas = {
+            "pit_stops": pa.schema(
+                [
+                    ("raceId", pa.int16()),
+                    ("driverId", pa.int16()),
+                    ("stop", pa.int16()),
+                    ("lap", pa.int16()),
+                    ("time", pa.string()),
+                    ("duration", pa.string()),
+                    ("milliseconds", pa.int32()),
+                ]
+            )
+        }
         with urllib.request.urlopen(url) as dl_file:
             with zipfile.ZipFile(io.BytesIO(dl_file.read())) as archive:
                 for i in ergast_assets:
                     with archive.open(i.file) as file:
-                        table = csv.read_csv(file)
+                        if i.asset_name in schemas:
+                            table = csv.read_csv(
+                                file,
+                                convert_options=ConvertOptions(
+                                    column_types=schemas.get(i.asset_name)
+                                ),
+                            )
+                        else:
+                            table = csv.read_csv(file)
                         yield Output(value=table, output_name=i.asset_name)
 
     return _parquet_files
--- src/duck_f1/transform/models/staging/ergast/stg_ergast__pit_stops.sql
+++ src/duck_f1/transform/models/staging/ergast/stg_ergast__pit_stops.sql
@@ -1,27 +1,33 @@
 with
-    raw_pit_stops as (select * from {{ source("src_ergast", "ergast__pit_stops") }}),
-    foreign_keys as (
-        select *
-        from raw_pit_stops ps
-        join
-            (
-                select driver_id, _ergast_driver_id from {{ ref("stg_ergast__drivers") }}
-            ) driver
-            on ps.driverid = driver._ergast_driver_id
-        join
-            (select session_id, _ergast_race_id from {{ ref("stg_ergast__sessions") }}) race
-            on ps.raceid = _session._ergast_race_id
-    ),
-    formatted as (
-        select
-            driver_id,
-            session_id,
-            stop as stop_count,
-            lap as lap_number,
-            time as pit_stop_time_label,
-            duration as pit_stop_duration_label,
-            milliseconds as lap_time_duration_ms
-        from foreign_keys
-    )
+raw_pit_stops as (select * from {{ source("src_ergast", "ergast__pit_stops") }}),
+
+driver_ids as (
+    select
+        driver_id,
+        _ergast_driver_id
+    from {{ ref("stg_ergast__drivers") }}
+),
+
+session_ids as (
+    select
+        session_id,
+        _ergast_race_id
+    from {{ ref("stg_sessions") }}
+    where session_type = 'race'
+),
+
+formatted as (
+    select
+        driver.driver_id,
+        _session.session_id,
+        pit_stop.stop::integer as stop_count,
+        pit_stop.lap::integer as lap_number,
+        pit_stop.duration as pit_stop_time_label,
+        to_milliseconds(pit_stop.milliseconds) as pit_stop_time
+    from raw_pit_stops as pit_stop
+    inner join driver_ids as driver on pit_stop.driverid = driver._ergast_driver_id
+    inner join session_ids as _session on pit_stop.raceid = _session._ergast_race_id
+)
+
 select *
 from formatted
